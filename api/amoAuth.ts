24fdb6679ff674e414da1a347031eae5548a1954
k.chistin <K.Chistin@zelenski.ru>
2025-07-08 21:00:49 +0300

    fix environment

--- api/amoAuth.ts
+++ api/amoAuth.ts
@@ -2,17 +2,16 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const {
-    CLIENT_ID,
-    REDIRECT_URI,
-    AMOCRM_SUBDOMAIN
+    CLIENT_AMO_ID,
+    REDIRECT_AMO_URI
   } = process.env;
 
-  if (!CLIENT_ID || !REDIRECT_URI || !AMOCRM_SUBDOMAIN) {
+  if (!CLIENT_AMO_ID || !REDIRECT_AMO_URI) {
     return res.status(500).send('Environment not fount');
   }
 
-  const authUrl = `https://${AMOCRM_SUBDOMAIN}.amocrm.ru/oauth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
-    REDIRECT_URI
+  const authUrl = `https://amocrm.ru/oauth?client_id=${CLIENT_AMO_ID}&redirect_uri=${encodeURIComponent(
+    REDIRECT_AMO_URI
   )}&response_type=code`;
 
   res.redirect(authUrl);
